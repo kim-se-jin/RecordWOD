79f9f312a60f1fce8848a76073d8c8784c88a957
kim-se-jin <>
2023-02-07 17:43:25 +0900

    구글 로그인 기능 구현 ( 원탭 로그인 )

--- app/src/main/java/com/sejin/recordwod/base/BaseFragment.kt
+++ app/src/main/java/com/sejin/recordwod/base/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -50,5 +51,19 @@ abstract class BaseFragment<VB : ViewBinding> :
 //    }
 
     protected abstract fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    private var progressBar: ProgressBar? = null
+
+    fun setProgressBar(bar: ProgressBar) {
+        progressBar = bar
+    }
+
+    fun showProgressBar() {
+        progressBar?.visibility = View.VISIBLE
+    }
+
+    fun hideProgressBar() {
+        progressBar?.visibility = View.INVISIBLE
+    }
 
 }
--- app/src/main/java/com/sejin/recordwod/fragment/LoginFragment.kt
+++ app/src/main/java/com/sejin/recordwod/fragment/LoginFragment.kt
@@ -1,13 +1,33 @@
 package com.sejin.recordwod.fragment
 
+import android.app.PendingIntent
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -23,12 +43,15 @@ import com.sejin.recordwod.R
 import com.sejin.recordwod.base.BaseFragment
 import com.sejin.recordwod.data.database.UserInfo
 import com.sejin.recordwod.databinding.FragmentLoginBinding
+import com.sejin.recordwod.view.viewmodel.LoginViewModel
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
     companion object {
         fun newInstance() = LoginFragment()
     }
+    //뷰모델
+
     // 데이터베이스
     val database = Firebase.database("https://recordwod-default-rtdb.firebaseio.com/")
     val myRef = database.getReference("users")
@@ -36,51 +59,43 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     // 카카오 로그인
     // 카카오계정으로 로그인 공통 callback 구성
     // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
-    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if (error != null) {
-            Log.e(TAG, "카카오계정으로 로그인 실패", error)
-        } else if (token != null) {
-            Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
-            GoMain()
+
+    // 구글로그인
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signInClient: SignInClient
+
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            handleSignInResult(result.data)
         }
-    }
+
+
     override fun initView() {
-        val context : Context = requireContext()
+        val context: Context = requireContext()
+
+        // 구글 로그인 -->
+        auth = Firebase.auth
+
+        // 활동을 초기화할 때 사용자가 현재 로그인 되어있는 지 확인
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+//            reload()
+        }
+        // <--
+
         // 버튼 클릭했을 때 로그인
         with(binding) {
-            KakaoLoginBtn.setOnClickListener {
+//            KakaoLoginBtn.setOnClickListener {}
 
-                // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-                if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-                    UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
-                        if (error != null) {
-                            Log.e(TAG, "카카오톡으로 로그인 실패", error)
-
-                            // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                            // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                            if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                                return@loginWithKakaoTalk
-                            }
-
-                            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
-                        } else if (token != null) {
-                            Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
-                            GoMain()
-                        }
-                    }
-                } else {
-                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
-                }
-
-
-            }
         }
+
+
         //<--
 
         // FireBase Realtime Database 쓰기
         WriteDataBase = Firebase.database.reference
-        CreateUser("1","안영서",true,true,true,"코너 크로스핏")
+        CreateUser("1", "안영서", true, true, true, "코너 크로스핏")
 
         // 읽기
         myRef.addValueEventListener(object : ValueEventListener {
@@ -88,8 +103,8 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
                 val userinfo = snapshot.child("user1")
 //                val value = dataSnapshot.getValue<String>()
 //                Log.d(TAG, "Value is: $value")
-                for(info in userinfo.children){
-                    Log.d("snap",info.toString())
+                for (info in userinfo.children) {
+                    Log.d("snap", info.toString())
                 }
             }
 
@@ -99,8 +114,15 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
         })
     }
 
-    private lateinit var WriteDataBase : DatabaseReference
-    fun CreateUser(userId: String, name:String, BBweight:Boolean, DBweight:Boolean, KBweight:Boolean, userBox:String ){
+    private lateinit var WriteDataBase: DatabaseReference
+    fun CreateUser(
+        userId: String,
+        name: String,
+        BBweight: Boolean,
+        DBweight: Boolean,
+        KBweight: Boolean,
+        userBox: String
+    ) {
         val newUser = UserInfo(userId, name, BBweight, DBweight, KBweight, userBox)
         WriteDataBase.child("users").child(userId).setValue(newUser)
     }
@@ -114,9 +136,142 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
         container: ViewGroup?
     ) = FragmentLoginBinding.inflate(inflater, container, false)
 
-    fun GoMain(){
-        // 로그인 -> 메인
-        Navigation.findNavController(binding.root).navigate(R.id.action_loginFragment_to_mainFragment)
+    // 로그인 -> 메인
+    fun GoMain() {
+        Navigation.findNavController(binding.root)
+            .navigate(R.id.action_loginFragment_to_mainFragment)
+    }
+
+    // 구글 로그인
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signInButton.setOnClickListener { signIn() }
+//        binding.signOutButton.setOnClickListener { signOut() }
+
+        signInClient = Identity.getSignInClient(requireContext())
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        // Display One-Tap Sign In if user isn't logged in
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            oneTapSignIn()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+
+    private fun handleSignInResult(data: Intent?) {
+        // Result returned from launching the Sign In PendingIntent
+        try {
+            // Google Sign In was successful, authenticate with Firebase
+            val credential = signInClient.getSignInCredentialFromIntent(data)
+            val idToken = credential.googleIdToken
+            if (idToken != null) {
+                Log.d(TAG, "firebaseAuthWithGoogle: ${credential.id}")
+                firebaseAuthWithGoogle(idToken)
+            } else {
+                // Shouldn't happen.
+                Log.d(TAG, "No ID token!")
+            }
+        } catch (e: ApiException) {
+            // Google Sign In failed, update UI appropriately
+            Log.w(TAG, "Google sign in failed", e)
+            updateUI(null)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        showProgressBar()
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+
+                hideProgressBar()
+            }
+    }
+
+
+    private fun signIn() {
+        val signInRequest = GetSignInIntentRequest.builder()
+            .setServerClientId(getString(com.firebase.ui.auth.R.string.default_web_client_id))
+            .build()
+
+        signInClient.getSignInIntent(signInRequest)
+            .addOnSuccessListener { pendingIntent ->
+                launchSignIn(pendingIntent)
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Google Sign-in failed", e)
+            }
+    }
+
+    private fun oneTapSignIn() {
+        // Configure One Tap UI
+        val oneTapRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            .build()
+
+        // Display the One Tap UI
+        signInClient.beginSignIn(oneTapRequest)
+            .addOnSuccessListener { result ->
+                launchSignIn(result.pendingIntent)
+            }
+            .addOnFailureListener { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+            }
+    }
+
+    private fun launchSignIn(pendingIntent: PendingIntent) {
+        try {
+            val intentSenderRequest = IntentSenderRequest.Builder(pendingIntent)
+                .build()
+            signInLauncher.launch(intentSenderRequest)
+        } catch (e: IntentSender.SendIntentException) {
+            Log.e(TAG, "Couldn't start Sign In: ${e.localizedMessage}")
+        }
+    }
+
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        signInClient.signOut().addOnCompleteListener(requireActivity()) {
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        hideProgressBar()
+//        if (user != null) {
+//            binding.signInButton.visibility = View.GONE
+//        }
+        GoMain()
     }
 }
 
